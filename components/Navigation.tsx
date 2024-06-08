cc56849fbb16fbdb6bb88d0b36b3d8b3ca43cbac
Junhao Zhang <howiejayzh@gmail.com>
2024-06-08 10:52:45 +0800

    fix: hide nav bar on mobile

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -4,11 +4,11 @@ import Link from 'next/link';
 export default function Navigation() {
   return (
     <nav>
-      <ul className="flex space-x-4 ">
-        {/* <li><Link href="/" className="hover:text-gray-200">Home</Link></li>
+      <ul className="hidden sm:flex space-x-4">
+        <li><Link href="/" className="hover:text-gray-200">Home</Link></li>
         <li><Link href="/about" className="hover:text-gray-200">About</Link></li>
         <li><Link href="/blog" className="hover:text-gray-200">Blog</Link></li>
-        <li><Link href="/contact" className="hover:text-gray-200">Contact</Link></li> */}
+        <li><Link href="/contact" className="hover:text-gray-200">Contact</Link></li>
       </ul>
     </nav>
   );

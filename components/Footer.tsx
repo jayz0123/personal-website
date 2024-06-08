c46e9813398a9e288e3e680190e64ec774580b6f
Junhao Zhang <howiejayzh@gmail.com>
2024-06-08 11:30:52 +0800

    fix: footer margin

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 // components/Footer.tsx
 export default function Footer() {
   return (
-    <footer className="bg-gradient-to-r from-slate-50 to-slate-500 dark:from-slate-950 dark:to-slate-500 py-4 rounded-full mx-6 mt-6 mb-6">
+    <footer className="py-4 rounded-full mx-6 mt-4 mb-4">
       <div className="container mx-auto text-center text-sm">
         © 2024 Junhao Zhang. Powered by enthusiasm.
       </div>
--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ import Networking from '@/components/Networking';
 // components/Header.tsx
 export default function Header() {
   return (
-    <header className="w-full py-4 px-8 rounded-full mx-0 mt-6 bg-gradient-to-r from-slate-50 to-slate-500 dark:from-slate-950 dark:to-slate-500">
+    <header className="w-11/12 py-4 px-8 rounded-full mx-0 mt-6 bg-gradient-to-r from-slate-50 to-slate-500 dark:from-slate-950 dark:to-slate-500">
       <div className="flex flex-row justify-between items-center">
         <h1 className="text-2xl font-bold">Howie Jayz</h1>
         <Navigation />

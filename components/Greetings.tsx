11a842d356faa46e30cec24deb9faf45944e7c7e
Junhao Zhang <howiejayzh@gmail.com>
2024-06-21 21:17:08 +0800

    fix: time logic for greetings

--- components/Greetings.tsx
+++ components/Greetings.tsx
@@ -1,31 +1,31 @@
-"use client";
+'use client';
 
-import getLocalTime from "@/utils/getLocalTime";
+import getLocalTime from '@/utils/getLocalTime';
 
 export default function Greetings() {
   const date = new Date();
   const currentTime = getLocalTime(date);
   const isAfternoon =
-    Number(currentTime.split(":")[0]) >= 12 &&
-    Number(currentTime.split(":")[0]) < 18;
+    Number(currentTime.split(':')[0]) >= 12 &&
+    Number(currentTime.split(':')[0]) < 19;
   const isEvening =
-    Number(currentTime.split(":")[0]) >= 18 &&
-    Number(currentTime.split(":")[0]) < 0;
+    Number(currentTime.split(':')[0]) >= 19 &&
+    Number(currentTime.split(':')[0]) < 24;
   const isMidnight =
-    Number(currentTime.split(":")[0]) >= 0 &&
-    Number(currentTime.split(":")[0]) < 5;
+    Number(currentTime.split(':')[0]) >= 0 &&
+    Number(currentTime.split(':')[0]) < 5;
 
   return (
     <h2 className="text-3xl mb-10">
       <span className="font-bold">
         {`${
           isMidnight
-            ? "Hey there, night wanderer! "
+            ? 'Hey there, night wanderer! '
             : isEvening
-            ? "Good evening! "
-            : isAfternoon
-            ? "Afternoon greetings! "
-            : "Morning! "
+              ? 'Good evening! '
+              : isAfternoon
+                ? 'Afternoon greetings! '
+                : 'Morning! '
         }`}
       </span>
       <span>
@@ -33,10 +33,10 @@ export default function Greetings() {
           isMidnight
             ? "Let's pretend staying up late is totally a good idea!"
             : isEvening
-            ? "Let's relax and do nothing—like the pros we are."
-            : isAfternoon
-            ? "Keep calm and fake productivity for a few more hours!"
-            : "The early bird gets the worm, but the second mouse gets the cheese!"
+              ? "Let's relax and do nothing—like the pros we are."
+              : isAfternoon
+                ? 'Keep calm and fake productivity for a few more hours!'
+                : 'The early bird gets the worm, but the second mouse gets the cheese!'
         }`}
       </span>
     </h2>

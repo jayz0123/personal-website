d7eaa5925dd0fbd009ee89777027e008203a7f0b
Junhao Zhang <howiejayzh@gmail.com>
2024-06-25 05:22:44 +0800

    feat: initialize supabase storage for photos

--- components/Photo.tsx
+++ components/Photo.tsx
@@ -1,34 +1,17 @@
-'use client';
-
-import Image from 'next/image';
+import { Image } from '@nextui-org/image';
+import NextImage from 'next/image';
 
 export default function Photo({ src }: { src: string }) {
   return (
-    <>
-      <Image
-        onClick={() =>
-          (document.getElementById(src) as HTMLDialogElement).showModal()
-        }
-        src={src}
-        width={300}
-        height={200}
-        className="rounded-box w-full h-auto max-h-full object-cover shadow-lg shadow-base-content hover:scale-[1.02] transition-transform cursor-pointer"
-        alt="photo"
-      />
-      <dialog id={src} className="modal">
-        <div className="modal-box w-11/12 max-w-7xl max-h-screen bg-transparent p-0">
-          <Image
-            src={src}
-            width={6000}
-            height={920}
-            className="rounded-box w-full h-auto max-h-screen object-contain"
-            alt="photo"
-          />
-        </div>
-        <form method="dialog" className="modal-backdrop">
-          <button>close</button>
-        </form>
-      </dialog>
-    </>
+    <Image
+      as={NextImage}
+      src={src}
+      width={300}
+      height={200}
+      quality={20}
+      // placeholder="blur"
+      className="rounded-box w-full h-auto max-h-full object-cover shadow-lg shadow-base-content hover:scale-[1.02] transition-transform cursor-pointer"
+      alt="photo"
+    />
   );
 }
--- components/PhotoCarousel.tsx
+++ components/PhotoCarousel.tsx
@@ -1,20 +1,48 @@
+import { createClient } from '@/utils/supabase/server';
 import Photo from './Photo';
 
-export default function PhotoCarousel() {
+export default async function PhotoCarousel() {
+  const supabase = createClient();
+  console.log(supabase);
+
+  // list all files in the requested folder
+  const { data: files, error: errorListing } = await supabase.storage
+    .from('Gallery')
+    .list('Japan', {
+      limit: 100,
+      offset: 0,
+      sortBy: { column: 'name', order: 'asc' },
+    });
+
+  if (!files) {
+    return <div>Error listing</div>;
+  }
+
+  // generate paths for all files
+  const paths = files.map((file) => 'Japan' + '/' + file.name);
+
+  // create signed urls for all files
+  const { data, error: errorCreating } = await supabase.storage
+    .from('Gallery')
+    .createSignedUrls(paths, 60);
+
+  if (!data) {
+    return <div>Error creating urls</div>;
+  }
+
+  const urls = data?.map((file) => file.signedUrl);
+
   return (
     <div className="carousel carousel-center glass p-4 space-x-8 rounded-box">
-      <div className="carousel-item">
-        <Photo src="/japan/JHZ08604.JPG" />
-      </div>
-      <div className="carousel-item">
-        <Photo src="/japan/DSCF0567.JPG" />
-      </div>
-      <div className="carousel-item">
-        <Photo src="/japan/DSCF0595.JPG" />
-      </div>
-      <div className="carousel-item">
-        <Photo src="/japan/JHZ08604.JPG" />
-      </div>
+      {urls.map((url: string, index: number) => (
+        <div className="carousel-item" key={index}>
+          <Photo src={url} />
+        </div>
+      ))}
     </div>
   );
+  // } catch (error) {
+  //   console.log(error);
+  //   return <div>Error fetching photos</div>;
+  // }
 }

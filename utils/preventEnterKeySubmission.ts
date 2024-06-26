cceff8c46bd231f0d35ab8dc83732819515b9948
Junhao Zhang <howiejayzh@gmail.com>
2024-06-26 21:08:53 +0800

    refactor: remove unused files

--- utils/preventEnterKeySubmission.ts
+++
@@ -1,6 +0,0 @@
-export default function preventEnterKeySubmission(e: React.KeyboardEvent<HTMLFormElement>) {
-  const target = e.target;
-  if (e.key === "Enter" && target instanceof HTMLInputElement) {
-    e.preventDefault();
-  }
-}

724568560a8d54d7e877458881d16c124bac2a3f
Junhao Zhang <howiejayzh@gmail.com>
2024-06-29 23:06:54 +0800

    chore: add new ui packages

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -2,13 +2,16 @@
 
 import { ThemeProvider } from 'next-themes';
 import { NextUIProvider } from '@nextui-org/system';
+import { ParallaxProvider } from 'react-scroll-parallax';
 import { useRouter } from 'next/navigation';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const router = useRouter();
   return (
     <ThemeProvider attribute="data-theme" defaultTheme="system" enableSystem>
-      <NextUIProvider navigate={router.push}>{children}</NextUIProvider>
+      <ParallaxProvider>
+        <NextUIProvider navigate={router.push}>{children}</NextUIProvider>
+      </ParallaxProvider>
     </ThemeProvider>
   );
 }

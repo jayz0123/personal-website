10971d47cd1e339f54df39cd06cb58f11acf339b
Junhao Zhang <howiejayzh@gmail.com>
2024-07-01 05:08:16 +0800

    fix: reduce image CLS

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -23,9 +23,12 @@ export default function Gallery() {
   ];
 
   return (
-    <div className="flex flex-col gap-y-16 items-center">
+    <div className="flex flex-col gap-y-16 items-center min-w-full">
       {places.map((place) => (
-        <div key={place.country} className="flex flex-col items-center">
+        <div
+          key={place.country}
+          className="flex flex-col items-center min-w-full"
+        >
           <h1 className="font-bold text-2xl uppercase">{place.country}</h1>
           <div className="divider divider-vertical mt-0" />
           <div className="min-w-full gap-2 grid grid-cols-12">

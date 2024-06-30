e1e518e6a28b85f151579094ea6750d0cde70ea9
Junhao Zhang <howiejayzh@gmail.com>
2024-07-01 04:43:02 +0800

    perf: use NextJS image component for faster loading

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -26,7 +26,7 @@ export default function Gallery() {
     <div className="flex flex-col gap-y-16 items-center">
       {places.map((place) => (
         <div key={place.country} className="flex flex-col items-center">
-          <h1 className="font-bold text-2xl">{place.country}</h1>
+          <h1 className="font-bold text-2xl uppercase">{place.country}</h1>
           <div className="divider divider-vertical mt-0" />
           <div className="min-w-full gap-2 grid grid-cols-12">
             {place.areas.map((area) => (
--- components/PhotoCard.tsx
+++ components/PhotoCard.tsx
@@ -1,5 +1,4 @@
-import NextImage from 'next/image';
-import { Image } from '@nextui-org/image';
+import Image from 'next/image';
 import { Card, CardHeader } from '@nextui-org/card';
 
 export default function PhotoCard({
@@ -10,24 +9,17 @@ export default function PhotoCard({
   area: string;
 }) {
   return (
-    <Card
-      isPressable
-      disableRipple
-      className="col-span-12 sm:col-span-4 h-[300px]"
-    >
-      <CardHeader className="backdrop-blur-3xl justify-center before:bg-white/10 border-white/20 border-1 overflow-hidden py-2 absolute before:rounded-xl rounded-large top-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-        <p className="text-white text-sm uppercase font-bold">
+    <Card isPressable className="col-span-12 md:col-span-4 h-[300px]">
+      <CardHeader className="w-fit justify-center overflow-hidden py-2 absolute bottom-1 left-1 ml-1 z-10">
+        <p className="text-white text-md uppercase font-bold">
           {area.replaceAll('_', ' ')}
         </p>
       </CardHeader>
       <Image
-        as={NextImage}
-        removeWrapper
-        isZoomed
         src={src}
         alt={area}
-        width={400}
-        height={400}
+        width={300}
+        height={300}
         className="z-0 w-full h-full object-cover"
       />
     </Card>

8c9a2ec143c60d4e9e8b9f981cc1b3115cf6a84d
Junhao Zhang <howiejayzh@gmail.com>
2024-08-01 21:31:45 +0800

    refactor: Update LoginButton component to remove unnecessary parameter in signIn function

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -14,7 +14,7 @@ export async function LoginButton() {
         'use server';
         if (loggedIn) await signOut();
 
-        await signIn('github');
+        await signIn();
       }}
       className="flex"
     >

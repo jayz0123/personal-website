572fb75c34a00854c920725550aa1fe50552f26c
Junhao Zhang <howiejayzh@gmail.com>
2024-08-06 00:07:02 +0800

    refactor: Optimize Timeline component with useMemo for timelineData

--- src/components/home/Timeline.tsx
+++ src/components/home/Timeline.tsx
@@ -1,4 +1,4 @@
-import { memo } from 'react';
+import { memo, useMemo } from 'react';
 
 import { ScrollShadow } from '@nextui-org/scroll-shadow';
 
@@ -6,55 +6,58 @@ import { AcademicCapIcon, BriefcaseIcon } from '@/components/ui/Icons';
 
 import { Glowing } from '../ui';
 
-const timelineData = [
-  {
-    year: '2016',
-    title: "BEng at Xi'an Jiaotong-Liverpool University",
-    description:
-      'Conquered the fundamentals of engineering, mastering complex calculations and innovative problem-solving. Graduated with top marks and an impressive collection of late-night study hacks.',
-    icon: 'academic',
-    alignment: 'start',
-  },
-  {
-    year: '2018',
-    title: 'BEng at University of Liverpool',
-    description:
-      'Delved into advanced engineering concepts, leading group projects and acing exams. Engineered brilliant solutions and survived on a strict diet of fish and chips. Left with honors and an impressive Beatles playlist.',
-    icon: 'academic',
-    alignment: 'end',
-  },
-  {
-    year: '2020',
-    title: 'MSc at Imperial College London',
-    description:
-      'Mastered machine learning and discovered the secret coffee spots of London. Walked away with a degree and a habit of speaking in algorithms.',
-    icon: 'academic',
-    alignment: 'start',
-  },
-  {
-    year: '2022',
-    title: 'Software Engineer at AMD',
-    description:
-      'Enhanced GPU performance and optimized code, ensuring smooth graphics for gamers worldwide. Became a debugging ninja and a master of efficient code.',
-    icon: 'briefcase',
-    alignment: 'end',
-  },
-  {
-    year: '2024',
-    title: 'PhD at University of Oxford',
-    description:
-      "Currently decoding the universe's secrets and occasionally rowing on the Thames. Mixing groundbreaking research with tea breaks and punting adventures.",
-    icon: 'academic',
-    alignment: 'start',
-  },
-  {
-    year: 'Present',
-    icon: 'academic',
-    alignment: 'start',
-  },
-];
-
 export const Timeline = memo(function Timeline() {
+  const timelineData = useMemo(
+    () => [
+      {
+        year: '2016',
+        title: "BEng at Xi'an Jiaotong-Liverpool University",
+        description:
+          'Conquered the fundamentals of engineering, mastering complex calculations and innovative problem-solving. Graduated with top marks and an impressive collection of late-night study hacks.',
+        icon: 'academic',
+        alignment: 'start',
+      },
+      {
+        year: '2018',
+        title: 'BEng at University of Liverpool',
+        description:
+          'Delved into advanced engineering concepts, leading group projects and acing exams. Engineered brilliant solutions and survived on a strict diet of fish and chips. Left with honors and an impressive Beatles playlist.',
+        icon: 'academic',
+        alignment: 'end',
+      },
+      {
+        year: '2020',
+        title: 'MSc at Imperial College London',
+        description:
+          'Mastered machine learning and discovered the secret coffee spots of London. Walked away with a degree and a habit of speaking in algorithms.',
+        icon: 'academic',
+        alignment: 'start',
+      },
+      {
+        year: '2022',
+        title: 'Software Engineer at AMD',
+        description:
+          'Enhanced GPU performance and optimized code, ensuring smooth graphics for gamers worldwide. Became a debugging ninja and a master of efficient code.',
+        icon: 'briefcase',
+        alignment: 'end',
+      },
+      {
+        year: '2024',
+        title: 'PhD at University of Oxford',
+        description:
+          "Currently decoding the universe's secrets and occasionally rowing on the Thames. Mixing groundbreaking research with tea breaks and punting adventures.",
+        icon: 'academic',
+        alignment: 'start',
+      },
+      {
+        year: 'Present',
+        icon: 'academic',
+        alignment: 'start',
+      },
+    ],
+    [],
+  );
+
   return (
     <Glowing variant="container">
       <ScrollShadow hideScrollBar className="min-w-fit h-[60svh]">
